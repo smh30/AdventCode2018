28353ab757d891559e91bbe2478450ed833d0bb0
Stephanie Hope <steph.hope@gmail.com>
2018-12-29 12:47:47 +1300

    Day5 part 1 done

---
+++ src/AdventCode/Day5Alchemy.java
@@ -0,0 +1,69 @@
+package AdventCode;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//I'm actually really happy at how this one went compared to previous days. Actually thought it through on paper beforehand
+//rather than just typing wildly and hoping something would work (at least, once i happened across the fact there was a
+//spare newline on the input....
+
+public class Day5Alchemy {
+    List<Character> allChars = new ArrayList<Character>();
+    List<Integer> indicesToDelete = new ArrayList<>();
+
+    public void start() {
+        getInput();
+
+        boolean done = false;
+        while (!done) {
+
+            indicesToDelete.clear();
+            findReactions();
+
+            doDelete();
+
+            if (indicesToDelete.size()==0){
+                done = true;
+            }
+        }
+
+        System.out.println(allChars.size());
+
+    }
+
+    public void doDelete(){
+        for (int i = allChars.size()-1; i >= 0; i--) {
+            if (indicesToDelete.contains(i)){
+                allChars.remove(i);
+            }
+        }
+
+    }
+
+    public void findReactions(){
+        for (int i = 1; i < allChars.size(); i++) {
+            if((allChars.get(i)-32 == allChars.get(i-1)||allChars.get(i)+32 == allChars.get(i-1))&& !indicesToDelete.contains(i-1)){
+                indicesToDelete.add(i-1);
+                indicesToDelete.add(i);
+            }
+        }
+
+    }
+
+    public void getInput() {
+        File input = new File("input05.html");
+        int x;
+        try (FileReader reader = new FileReader(input)) {
+            while ((x = reader.read()) != -1) {
+                allChars.add((char) x);
+            }
+        } catch (
+                IOException e) {
+            System.out.println("an IO exception with the input");
+        }
+
+    }
+}
